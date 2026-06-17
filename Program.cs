e64a4cbb700f6c32bc3d1e90fe19ea835fd6785f
eagarciab-ux <eagarciab@uamv.edu.ni>
2026-06-16 20:46:47 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+static void Main()
+{
+    int opcion;
+
+    do
+    {
+        Console.WriteLine("\n===== UAM JAGUAR MARKET =====");
+        Console.WriteLine("1. Registrar emprendimiento");
+        Console.WriteLine("2. Salir");
+        Console.Write("Seleccione una opción: ");
+
+        opcion = int.Parse(Console.ReadLine());
+
+        switch (opcion)
+        {
+            case 1:
+                RegistrarEmprendimiento();
+                break;
+
+            case 2:
+                Console.WriteLine("Saliendo del sistema...");
+                break;
+
+            default:
+                Console.WriteLine("Opción no válida.");
+                break;
+        }
+
+    } while (opcion != 2);
+}
