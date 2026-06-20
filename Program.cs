c5901eeeb556f1e62758949aeeba89a434196786
NORAELENA KARELIA URROZ LOPEZ <nkurroz@uamv.edu.ni>
2026-06-20 12:03:52 -0600

    Ajustar la cantidad máxima de stands

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace SistemaJaguarMarket
         }
 
         // 2. TODOS DEBEN USAR ESTAS MISMAS VARIABLES (Mismos nombres)
-        const int MAX_STANDS = 100;
+        const int MAX_STANDS = 40;
         static Emprendimiento[] listaMarket = new Emprendimiento[MAX_STANDS];
         static int contadorEmprendimientos = 0;
 
